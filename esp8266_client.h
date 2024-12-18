c842283d10e886fa50ae02419cd9af14aa986d06
Aby293 <abdelrahmanelaby@gmail.com>
2024-12-18 22:02:24 +0200

    Cleaned repo

--- esp8266_client.h
+++
@@ -1,6 +0,0 @@
-#ifndef ESP8266_CLIENT_H
-#define ESP8266_CLIENT_H
-
-void connect_to_server();
-
-#endif // ESP8266_CLIENT_H
--- esp8266_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef ESP8266_SERVER_H
-#define ESP8266_SERVER_H
-
-void start_server();
-
-#endif // ESP8266_SERVER_H
